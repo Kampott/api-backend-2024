5a4f696374939ca8b0d0725e24dbbc0280000bde
Kampot <anis.bouslama.ru@gmail.com>
2024-11-18 17:32:26 +0300

    packages installed

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from backend import routers
 from backend.config import settings
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -12,4 +12,4 @@ if len(settings.CORS_ORIGINS) > 0:
 routers.init_app(app)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=settings.PORT, reload=True)
+    uvicorn.run('backend.main:app', host='127.0.0.1', port=settings.PORT, reload=True)
